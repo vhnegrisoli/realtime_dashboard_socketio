2e731ab920e07815cec25e250ad33df7ebb55abf
Victor Hugo Negrisoli <victornegrisoli@xbrain.com.br>
2020-08-14 16:29:00 -0300

    Adiciona handlebars e inicia front-end

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-import express from "express";
-import http from "http";
-import io from "socket.io";
-import handlebars from "express-handlebars";
+import express from 'express';
+import cors from 'cors';
+import http from 'http';
+import io from 'socket.io';
+import handlebars from 'express-handlebars';
 
 const app = express();
 http.createServer(app);
 const socket = io(http);
 
-app.engine("handlebars", handlebars());
-app.set("view engine", "handlebars");
+app.use(express.json());
+app.use(cors());
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
 
-socket.on("connection", (socket) => {
-  console.log(" connected");
+socket.on('connection', (socket) => {
+  console.log(' connected');
 });
 
-app.get("/", (req, res) => {
-  socket.emit("event", {
-    someProperty: "some value",
-    otherProperty: "other value",
+app.post('/teste-form', (req, res) => {
+  console.log(req.body);
+  socket.emit('event', {
+    someProperty: 'some value',
+    otherProperty: 'other value',
   });
-  return res.json({ message: "Test" });
+  res.render('layouts/main', { body: 'Olá, mundo!' });
+});
+
+app.get('/', (req, res) => {
+  return res.render('layouts/main');
 });
 
 app.listen(8080, () => {
-  console.log("Conectado");
+  console.log('Conectado');
 });
