7985a2cd5b6a405f4f1c79317481b63b3e8abc7a
Victor Hugo Negrisoli <victorhugonegrisoli.ccs@hotmail.com>
2020-08-14 22:27:31 -0300

    Adiciona primeiro gráfico em tempo real com socket.io

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
-import express from 'express';
-import cors from 'cors';
-import http from 'http';
-import io from 'socket.io';
-import handlebars from 'express-handlebars';
+import express from "express";
+import http from "http";
+import io from "socket.io";
+import bodyParser from "body-parser";
+import cors from "cors";
+import handlebars from "express-handlebars";
 
 const app = express();
-http.createServer(app);
-const socket = io(http);
+const server = http.Server(app);
+const socketIO = io(server);
 
-app.use(express.json());
+app.use(express.static("public"));
+app.use(bodyParser.json());
 app.use(cors());
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
 
-socket.on('connection', (socket) => {
-  console.log(' connected');
+app.post("/", (req, res) => {
+  const { labels, data } = req.body;
+  let values = {
+    labels,
+    data,
+  };
+  socketIO.emit("dados", values);
+  return res.json({ message: "Enviado!" });
 });
 
-app.post('/teste-form', (req, res) => {
-  console.log(req.body);
-  socket.emit('event', {
-    someProperty: 'some value',
-    otherProperty: 'other value',
+app.get("/dashboard", (req, res) => {
+  return res.render("dashboard", { layout: false });
+});
+
+socketIO.on("connection", function (socket) {
+  console.log("Usuário " + socket.id + " conectado.");
+  socket.on("disconnect", (reason) => {
+    console.log("Usuário " + socket.id + " desconectado.");
   });
-  res.render('layouts/main', { body: 'Olá, mundo!' });
 });
 
-app.get('/', (req, res) => {
-  return res.render('layouts/main');
-});
-
-app.listen(8080, () => {
-  console.log('Conectado');
-});
+server.listen(8080);
