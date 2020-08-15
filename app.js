12e25fec751a864d0f8eefda25f66a5bf13a36cc
Victor Hugo Negrisoli <victorhugonegrisoli.ccs@hotmail.com>
2020-08-15 15:48:31 -0300

    Melhora layout da dashboard e adiciona chamada REST com axios

--- app.js
+++ app.js
@@ -1,38 +1,60 @@
-import express from 'express';
-import http from 'http';
-import io from 'socket.io';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import handlebars from 'express-handlebars';
+import express from "express";
+import http from "http";
+import io from "socket.io";
+import bodyParser from "body-parser";
+import cors from "cors";
+import handlebars from "express-handlebars";
 
 const app = express();
 const server = http.Server(app);
 const socketIO = io(server);
 
-app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+app.engine("handlebars", handlebars());
+app.engine("handlebars", handlebars({ defaultLayout: false }));
+app.set("view engine", "handlebars");
+app.use(express.static("public"));
 
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
   const { labels, data } = req.body;
   let values = {
     labels,
     data,
   };
-  socketIO.emit('dados', values);
-  return res.json({ message: 'Enviado!' });
+  socketIO.emit("dados", values);
+  return res.json({ message: "Enviado!" });
 });
 
-app.get('/dashboard', (req, res) => {
-  return res.render('dashboard', { layout: false, card1: 0, card2: 0, card3: 0, card4: 0 });
+app.post("/update-data", (req, res) => {
+  console.log("Tô no servidor");
+  console.log(req.body);
+  return res.render("dashboard", {
+    card1: 0,
+    card2: 0,
+    card3: 0,
+    card4: 0,
+  });
 });
 
-socketIO.on('connection', function (socket) {
-  console.log('Usuário ' + socket.id + ' conectado.');
-  socket.on('disconnect', (reason) => {
-    console.log('Usuário ' + socket.id + ' desconectado.');
+app.get("/dashboard", (req, res) => {
+  return res.render("dashboard", {
+    card1: 0,
+    card2: 0,
+    card3: 0,
+    card4: 0,
+  });
+});
+
+app.get("/", (req, res) => {
+  return res.render("main");
+});
+
+socketIO.on("connection", function (socket) {
+  console.log("Usuário " + socket.id + " conectado.");
+  socket.on("disconnect", (reason) => {
+    console.log("Usuário " + socket.id + " desconectado.");
   });
 });
 
