fc52f6cd2a7a2d70989f724f6177d340e4ad645a
Victor Hugo Negrisoli <victorhugonegrisoli.ccs@hotmail.com>
2020-08-26 11:33:45 -0300

    Finaliza CRUD de categorias

--- src/modulos/produto/controller/CategoriaController.js
+++ src/modulos/produto/controller/CategoriaController.js
@@ -24,17 +24,87 @@ class CategoriaController {
     return res.json(categorias);
   }
 
+  async buscarCategoria(req, res) {
+    const { id } = req.params;
+    if (!id) {
+      return res
+        .status(400)
+        .json({ message: "A categoria não foi encontrada" });
+    }
+    const categoria = await Categoria.findById(id);
+    return res.json(categoria);
+  }
+
   async salvarCategoria(req, res) {
     const { descricao } = req.body;
     const categoria = await Categoria.findOne({ descricao });
+    if (!descricao) {
+      return res
+        .status(400)
+        .json({ message: "A descrição da categoria é obrigatória." });
+    }
     if (categoria) {
-      console.log(categoria);
       return res
         .status(400)
         .json({ message: "Esta categoria já está cadastrada." });
     }
-    const novaCategoria = await Categoria.create({ descricao });
-    return res.json(novaCategoria);
+    try {
+      const novaCategoria = await Categoria.create({ descricao });
+      return res.json(novaCategoria);
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ message: "Houve um erro ao salvar a categoria." });
+    }
+  }
+
+  async editarCategoria(req, res) {
+    const { id } = req.params;
+    const { descricao } = req.body;
+    if (!id) {
+      return res.status(400).json({ message: "O id é obrigatório." });
+    }
+    if (!descricao) {
+      return res
+        .status(400)
+        .json({ message: "A descrição da categoria é obrigatória." });
+    }
+    const categoriaDescricao = await Categoria.findOne({ descricao });
+    const categoria = await Categoria.findById(id);
+    if (
+      categoriaDescricao &&
+      descricao === categoriaDescricao.descricao &&
+      String(id) === String(categoriaDescricao._id)
+    ) {
+      return res
+        .status(400)
+        .json({ message: "Esta categoria já está cadastrada." });
+    }
+    categoria.descricao = descricao;
+    try {
+      await categoria.save();
+      return res.json("A categoria foi atualizada com sucesso!");
+    } catch (error) {
+      return res
+        .status(400)
+        .json({ message: "Houve um erro ao editar a categoria." });
+    }
+  }
+
+  async removerCategoria(req, res) {
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ message: "O id é obrigatório." });
+    }
+    try {
+      await Categoria.findByIdAndRemove(id);
+      return res.json("A categoria foi removida com sucesso!");
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(400)
+        .json({ message: "Houve um erro ao remover a categoria." });
+    }
   }
 }
 
--- src/modulos/produto/routes/categoriaRoutes.js
+++ src/modulos/produto/routes/categoriaRoutes.js
@@ -5,9 +5,19 @@ import CategoriaController from "../controller/CategoriaController";
 const router = new Router();
 
 router.get("/categorias/listar", CategoriaController.buscarCategorias);
-router.get("/categorias/nova", CategoriaController.iniciarFormularioCategoria);
+router.get(
+  "/categorias/cadastrar",
+  CategoriaController.iniciarFormularioCategoria
+);
+router.get(
+  "/categorias/cadastrar/:id",
+  CategoriaController.iniciarFormularioCategoria
+);
 
 router.get("/api/categorias", CategoriaController.buscarTodasCategorias);
+router.get("/api/categoria/:id", CategoriaController.buscarCategoria);
 router.post("/api/categorias", CategoriaController.salvarCategoria);
+router.put("/api/categorias/:id", CategoriaController.editarCategoria);
+router.delete("/api/categorias/:id", CategoriaController.removerCategoria);
 
 export default router;
