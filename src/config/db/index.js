58cca4c0010ebebaa5ab6c6199a664d43d3188b9
Victor Hugo Negrisoli <victorhugonegrisoli.ccs@hotmail.com>
2020-08-28 11:07:53 -0300

    Adiciona URI do MongoDB

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -4,6 +4,8 @@ const env = process.env.NODE_ENV;
 import * as url from "../util/url";
 
 export function connect() {
+  console.log(env);
+  console.log(url.MONGO_DB_CONNECTION);
   mongoose.connect(url.MONGO_DB_CONNECTION, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
