4ec91c543af93230f53243d0109244c8e5f7f82e
Victor Hugo Negrisoli <victorhugonegrisoli.ccs@hotmail.com>
2020-11-14 20:02:53 -0300

    Altera banco MongoDB mLab para Atlas

--- src/config/util/url.js
+++ src/config/util/url.js
@@ -4,5 +4,5 @@ export const MONGO_DB_CONNECTION =
   env === "dev"
     ? "mongodb://localhost:27017/dashboard_realtime"
     : env === "prod"
-    ? "mongodb://heroku_wmt175dj:aihk8kenne0u2qmag6vi4v5n2o@ds061298.mlab.com:61298/heroku_wmt175dj"
+    ? "mongodb+srv://biot_admin:biot_admin@cluster0.jcts2.mongodb.net/dashboard_realtime?retryWrites=true&w=majority"
     : "mongodb://socketio_dashboard-mongo:27017/dashboard_realtime";
